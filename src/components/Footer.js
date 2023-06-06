5c31905e63af300a99a1cc808f0a098d2ca09c77
Gabi <86157167+GabrielPetrov@users.noreply.github.com>
2023-06-06 15:14:11 +0300

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "./Button";
+// import { Button } from "./Button";
 import './Footer.css'
 import { Link } from "react-router-dom";
 
@@ -12,22 +12,21 @@ function Footer(){
                 </p>
                 <div className="input-areas">
                     <form>
-                        <input type="email" name="email" placeholder="Your Email" className="footer-input"/>
-                        <Button buttonStyle='btn--outline'>Subscribe</Button>
+                        <input type="email" id="email" placeholder="Enter your email" required className="footer-input"/>
+                        <input  type="submit" value = "Subscribe"></input>
                     </form>
                 </div>
             </section>
             <div className="footer-links">
                 <div className="footer-link-wrapper">
-                    <div className="footer-link-items">
-                        <h2>About Us</h2>
-                        <Link to='/'>How it works</Link>
-                        <Link to='/'>Careers</Link>
-                        <Link to='/'>Investors</Link>
-                        <Link to='/'>Terms of services</Link>
+                    <h2>About Us</h2>
+                    <div className="footer-link-items"> 
+                        <a href='https://docs.google.com/document/d/17dSrMscUVxfHInJeMXnCCa7kSOfHcMLVDgmWYKGkyYk/edit' className = "info-links" target="_blank">How it works</a>
+                        <a href='https://www.tiktok.com/@daniel.dimitrov.2021?is_from_webapp=1&sender_device=pc' className = "info-links" target="_blank">Investors</a>
+                        <a href='https://policies.google.com/terms?hl=en-US' className = "info-links" target="_blank">Terms of services</a>
                     </div>
                 </div>
-            </div>
+            </div>  
         </div>
     )
 }

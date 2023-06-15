24c95a8cf0519aa4312b7da0eb980723d25fba3f
Nikieprogramach <101349864+Nikieprogramach@users.noreply.github.com>
2023-06-15 13:46:16 +0300

    Added event page
    Added event page

--- src/components/pages/Events.js
+++ src/components/pages/Events.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../../App.css'
 import './Events.css'
 import Cards from "../Cards";
+import { Button } from "../Button";
 
 export default function Events(){
     return(
@@ -10,6 +11,9 @@ export default function Events(){
                 <h1 className="events-title">Events</h1>
                 <div className="event-list-container">
                     <Cards/>
+                    <div className="create-event-button">
+                        <Button buttonStyle='btn--outline' path='/create-event'>Create event</Button>
+                    </div>
                 </div>
             </div>
         </div>
